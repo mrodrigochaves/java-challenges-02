f2c06bf9261da40ed32a91426fcbaf99b6e0dd21
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-17 20:34:57 -0300

    resolving problem

--- resolution/src/Main.java
+++ resolution/src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Main {
@@ -9,11 +8,17 @@ public class Main {
         float porcentagemDesconto = 0.0f;
         float valorDesconto = 0.0f;
 
+        float valorPagar = 0.0f;
+
+        int totalSemiNovos = 0;
+        int totalCarros = 0;
+
         Scanner leitor = new Scanner(System.in);
 
-        boolean desejaRepetir = true;
+        char desejaRepetir = 's';
 
-        while (desejaRepetir == true) {
+        while(desejaRepetir == 's') {
+
             System.out.println("Digite o ano de fabricação do veículo: ");
             anoVeiculo = leitor.nextInt();
 
@@ -21,14 +26,25 @@ public class Main {
             valorVeiculo = leitor.nextFloat();
 
             if (anoVeiculo <= 2000) {
-                porcentagemDesconto = 0.012f;
+                porcentagemDesconto = 0.12f;
             } else {
-                porcentagemDesconto = 0.007f;
+                porcentagemDesconto = 0.07f;
+                totalSemiNovos++;
             }
 
+            totalCarros++;
+
             valorDesconto = valorVeiculo * porcentagemDesconto;
+            valorPagar = valorVeiculo - valorDesconto;
+
+            System.out.println("Valor do desconto: " + valorDesconto);
+            System.out.println("Valor do veículo: " + valorPagar);
 
             System.out.println("Deseja cadastrar mais? S = sim / N = não ");
+            desejaRepetir = leitor.next().charAt(0);
         }
+
+        System.out.println("Total de seminovos: " + totalSemiNovos);
+        System.out.println("Total de carros: " + totalCarros);
     }
 }
